bb39b97c7fc2d27321f26e2eedd10efec05c0a37
hung1451020115 <nguyenviethungqt20@gmail.com>
2021-09-11 10:31:14 +0700

    first commit

---
+++ hung.py
@@ -0,0 +1,9 @@
+print("nhập 2 số")
+print("nhập a")
+a = input()
+print("nhập b")
+b = input()
+if a > b:
+    print("a lon hon b")
+else:
+    print("a nho  hon b")
---
+++ songuyento.py
@@ -0,0 +1,12 @@
+print("nhap vao 1 so")
+a = int(input())
+if a < 2:
+    print("a không là số nguyên tố")
+if (a == 2):
+    print(a,"là số nguyên tố")
+else:
+    for i in range(3,n,1):
+        if (n % i ==0):2
+            print("a không là số nguyên tố")
+        else:
+            print("a là số nguyên tố")
